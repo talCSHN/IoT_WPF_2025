216190d0c55daf9a329ef31a921c709848bf746f
talCSHN <yujakinasakoon@gmail.com>
2025-05-14 16:52:00 +0900

    day05 Commit

--- README.md
+++ README.md
@@ -287,5 +287,51 @@ WPF Study
 - BooksView, BooksViewModel 작업 실습
 
 ## 5일차
+### MovieFinder 2025
+- 전체 UI : UI설계화면 다섯영역으로 구분
+
+    <img src="./image/wpf0014.png" width="650">
+
+- 영화즐겨찾기앱
+    - TMDB 사이트에서 제공하는 OpenAPI로 데이터 가져오기
+    - 내가 좋아하는 영화리스트 선택, 즐겨찾기 저장
+    - 저장한 영화만 리스트업, 삭제 가능
+    - 선택된 영화 더블클릭 > 영화 상세정보 팝업
+    - 선택된 영화 선택 > 예고편보기 > 유튜브동영상 팝업
+
+- API / OpenAPI
+    - Applicaiton Programming Interface
+    - 개발자가 직접개발하지 않고 제3자가 만들어서 제공하는 서비스
+
+- TMDB, Youtube
+    - [TMDB](https://www.themoviedb.org/) API 신청
+    - [Youtube Data API](https://console.cloud.google.com/) 신청    
+        - 프로젝트 생성 후 API 및 서비스 > 라이브러리
+        - Youtube Data API v3 선택
+        - 사용버튼 클릭 
+        - 사용자 인증정보 입력
+
+### 프로젝트 시작
+1. WPF 프로젝트 생성
+2. NuGet 패키지 사용할 기본 라이브러리 설치
+    - CommunityToolkit.Mvvm
+    - MahApps.Metro / MahApps.Metro.IconPacks
+    - MySql.Data
+    - NLog
+3. 폴더생성 : Helpers, Models, Views, ViewModels
+4. MVVM 구조 초기작업
+5. UI 구현
+
+    <img src="./image/wpf0015.png" width="650">
+
+6. 로직구현
+    1. TMDB API 사용 구현
+    2. 관련 기능 전부구현
+7. 데이터그리드 더블클릭해서 상세정보 표시
+    - NuGet 패키지에서 Microsoft.Xaml.Behaviors.Wpf 설치
+8. 텍스트박스에서 엔터시 이벤트 발생 처리
+9. 텍스트박스 한글 입력 우선 처리
+10. 실행시 텍스트박스에 포커스 가도록 처리
+
 
 ## 6일차
---
+++ day05/Day05Study/2025-MovieFinder/ViewModels/MoviesViewModel.cs
@@ -0,0 +1,140 @@
+﻿using _2025_MovieFinder.Helpers;
+using _2025_MovieFinder.Models;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using MahApps.Metro.Controls.Dialogs;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace _2025_MovieFinder.ViewModels
+{
+    public partial class MoviesViewModel : ObservableObject
+    {
+        private readonly IDialogCoordinator dialogCoordinator;
+        public MoviesViewModel(IDialogCoordinator coordinator)
+        {
+            this.dialogCoordinator = coordinator;
+            Common.LOGGER.Info("MovieFinder2025 Start");
+
+            PosterUri = new Uri("/No_Picture.png", UriKind.RelativeOrAbsolute);
+        }
+
+        private string _movieName;
+
+        public string MovieName { 
+            get => _movieName;
+            set => SetProperty(ref _movieName, value); 
+        }
+
+        private ObservableCollection<MovieItem> _movieItems;
+        public ObservableCollection<MovieItem> MovieItems { 
+            get => _movieItems; 
+            set => SetProperty(ref _movieItems, value);
+        }
+
+        private MovieItem _selectedMovieItem;
+        public MovieItem SelectedMovieItem
+        {
+            get => _selectedMovieItem;
+            set 
+            {
+                SetProperty(ref _selectedMovieItem, value);
+                Common.LOGGER.Info($"Selected Movie Item : {value.Poster_path}");
+                PosterUri = new Uri($"{_base_url}{value.Poster_path}", UriKind.RelativeOrAbsolute);
+            } 
+        }
+
+        private string _base_url = "https://image.tmdb.org/t/p/w300_and_h450_bestv2";
+
+        private Uri _posterUri; 
+        public Uri PosterUri { 
+            get => _posterUri; 
+            set => SetProperty(ref _posterUri, value);
+        }
+
+        [RelayCommand]
+        public async Task SearchMovie()
+        {
+            //await this.dialogCoordinator.ShowMessageAsync(this, "영화 검색", MovieName);
+            if (string.IsNullOrEmpty(MovieName))
+            {
+                await this.dialogCoordinator.ShowMessageAsync(this, "영화 검색", "영화 제목 입력");
+                return;
+            }
+
+            var controller = await dialogCoordinator.ShowProgressAsync(this, "대기중", "검색 중...");
+            controller.SetIndeterminate();
+            SearchMovie(MovieName);
+            await Task.Delay(500);
+            await controller.CloseAsync();
+        }
+
+        private async void SearchMovie(string movieName)
+        {
+            string tmdb_apiKey = "ad3884608731e93c48ae539f6fa5a37a";   // TMDB에서 신청한 API키
+            string encoding_movieName = HttpUtility.UrlEncode(movieName, Encoding.UTF8);    // 입력한 한글을 UTF-8로 변경
+            string openApiUri = $"https://api.themoviedb.org/3/search/movie?api_key={tmdb_apiKey}" +
+                                $"&language=ko-KR&page=1&include_adult=false&query={encoding_movieName}";
+            //Debug.WriteLine(openApiUri);
+            Common.LOGGER.Info($"TMDB URI : {openApiUri}");
+            string result = string.Empty;
+
+            // OpenAPI 실행할 웹 객체. WebRequest, WebResponse -> Deprecated: 추후 삭제될 예정
+            //WebRequest req = null;
+            //WebResponse res = null;
+            HttpClient client = new HttpClient();
+            ObservableCollection<MovieItem> movieItems = new ObservableCollection<MovieItem>();
+            //Task<MovieSearchResponse?> response;
+            //HttpResponseMessage response;
+            string reader; // 응답 결과를 담는 객체
+
+            try
+            {
+                //response = await client.GetAsync(openApiUri);
+                var response = await client.GetFromJsonAsync<MovieSearchResponse>(openApiUri);
+               
+                //result = await response.Content.ReadAsStringAsync();
+                foreach (var movie in response.Results)
+                {
+                    Common.LOGGER.Info($"{movie.Title} {movie.Release_date.ToString("yyyy-MM-dd")}");
+                    movieItems.Add(movie);
+                }
+                //Common.LOGGER.Info($"API RESULT : {result}");
+               
+                //await this.dialogCoordinator.ShowMessageAsync(this, "오류", "API요청 실패");
+                //Common.LOGGER.Info($"API 요청 실패");
+                
+            }
+            catch (Exception ex)
+            {
+                await this.dialogCoordinator.ShowMessageAsync(this, "예외" ,ex.Message);
+                Common.LOGGER.Fatal(ex.Message);
+            }
+            MovieItems = movieItems;    // View에 가져갈 속성에 데이터 할당
+        }
+
+        [RelayCommand]
+        public async Task MovieItemDoubleClick()
+        {
+            var currentMovie = SelectedMovieItem;
+            if (currentMovie != null)
+            {
+                StringBuilder sb = new StringBuilder();
+                sb.Append($"{currentMovie.Original_title} ({currentMovie.Release_date.ToString("yyyy-MM-dd")})\n");
+                sb.Append($"평점 : {currentMovie.Vote_average.ToString("F2")}\n");
+                sb.Append("\n" + currentMovie.Overview);
+                await this.dialogCoordinator.ShowMessageAsync(this, currentMovie.Title, sb.ToString());
+            }
+        }
+    }
+}
